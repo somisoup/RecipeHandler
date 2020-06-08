1e09aeb4d72230343f8b65fb2e90c33eefaf8a20
somisoup <somisoup@gmail.com>
2020-06-08 15:47:20 -0400

    Fixed crash when going to Recipe page

--- app/src/main/java/com/example/recipehandler/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/recipehandler/ui/search/SearchFragment.kt
@@ -4,14 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.example.recipehandler.MainActivity
 import com.example.recipehandler.R
-import com.example.recipehandler.classes.Recipe
 import com.example.recipehandler.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
@@ -27,7 +23,6 @@ class SearchFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
         searchViewModel =
                 ViewModelProviders.of(this).get(SearchViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_search, container, false)
         binding.searchViewModel = searchViewModel
         return binding.root
     }
--- app/src/main/java/com/example/recipehandler/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/recipehandler/ui/search/SearchViewModel.kt
@@ -8,7 +8,7 @@ import com.example.recipehandler.classes.Recipe
 class SearchViewModel : ViewModel() {
     val searchRecipe = Recipe()
     var searchRating : Float = 0.0F
-    lateinit var totalTime: String
+    var totalTime: String = ""
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is the Search Recipes Fragment"
